2de3dc8c00a6d9f1253cfe0bf0d09bd3545c82cd
Pavel V <pavel@299792458.ru>
2020-11-03 10:32:25 +0300

    meh

--- bl/_mdgen.sh
+++ bl/_mdgen.sh
@@ -2,7 +2,7 @@ for f in *.txt; do
     echo $f
     cat $f | sed -n '/##/,/*/p' | egrep -v category >$f.md
     title=$(cat $f | sed -n '/##/,/*/p' | egrep -v category | egrep '##' | cut -f2- -d' ')
-    link=$f
+    link=$f.md
     cat >>index.md << EOF
 * [$title]($link)
 EOF
